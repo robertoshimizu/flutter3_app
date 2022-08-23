c497e53f62ee9648ab85f371d519aa297f254f72
Roberto Shimizu <rshimizubr@gmail.com>
2022-08-23 19:22:07 -0300

    test: Test post request with mock return sucess

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 
 import 'package:faker/faker.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter3_app/data/usecases/usecases.dart';
+import 'package:flutter3_app/domain/usecases/usecases.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
@@ -13,7 +16,7 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
 
-  Future<void> request({
+  Future<dynamic>? request({
     required String url,
     required String method,
     Map? body,
@@ -22,19 +25,29 @@ class HttpAdapter {
       "content-type": "application/json",
       "accept": "application/json"
     };
-    if (method == "post") {
-      await client.post(
-        Uri.parse(url),
-        headers: headers,
-        body: null,
-        encoding: null,
-      );
-    }
-    if (method == "get") {
-      await client.get(
-        Uri.parse(url),
-        headers: headers,
-      );
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    try {
+      if (method == "post") {
+        http.Response response = await client.post(
+          Uri.parse(url),
+          headers: headers,
+          body: jsonBody,
+          encoding: null,
+        );
+        // debugPrint(response.body);
+        return response.body.isEmpty ? null : jsonDecode(response.body);
+      }
+      if (method == "get") {
+        var response = await client.get(
+          Uri.parse(url),
+          headers: headers,
+        );
+        // debugPrint(response.body);
+        return jsonDecode(response.body);
+      }
+      return {"userId": 1, "id": 2, "title": "mock"};
+    } catch (e) {
+      debugPrint(e.toString());
     }
   }
 }
@@ -45,10 +58,26 @@ void main() {
   late HttpAdapter sut;
   late String url;
 
+  late String email;
+  late String password;
+  late AuthenticationParams authenticationParams;
+  late Map body;
+
+  late String accessToken;
+  late String name;
+
   setUp(() {
     client = mocks.MockClient();
     sut = HttpAdapter(client);
     url = faker.internet.httpUrl();
+
+    email = faker.internet.email();
+    password = faker.internet.password();
+    authenticationParams = AuthenticationParams(email, password);
+    body = RemoteAuthenticationParams.fromDomain(authenticationParams).toJson();
+
+    accessToken = faker.guid.guid();
+    name = faker.person.name();
   });
 
   group('post', (() {
@@ -60,13 +89,28 @@ void main() {
           "content-type": "application/json",
           "accept": "application/json"
         },
-        body: null,
+        body: jsonEncode(body),
         encoding: null,
       )).thenAnswer((_) async => http.Response(
-          '{"accessToken": ${faker.guid.guid()}, "name": ${faker.person.name()}}',
-          200));
+          '{"accessToken": "$accessToken", "name": "$name"}', 200));
 
-      await sut.request(url: url, method: 'post');
+      final response = await sut.request(url: url, method: 'post', body: body);
+      expect(response, {"accessToken": "$accessToken", "name": "$name"});
+    });
+    test('Should return null if post returns 200 with no data', () async {
+      // Stub
+      when(client.post(
+        Uri.parse(url),
+        headers: {
+          "content-type": "application/json",
+          "accept": "application/json"
+        },
+        body: jsonEncode(body),
+        encoding: null,
+      )).thenAnswer((_) async => http.Response('', 200));
+
+      final response = await sut.request(url: url, method: 'post', body: body);
+      expect(response, null);
     });
 
     test('get request', () async {
@@ -76,7 +120,8 @@ void main() {
       })).thenAnswer((_) async =>
           http.Response('{"userId": 1, "id": 2, "title": "mock"}', 200));
 
-      await sut.request(url: url, method: 'get');
+      final response = await sut.request(url: url, method: 'get');
+      expect(response, {"userId": 1, "id": 2, "title": "mock"});
     });
   }));
 }
